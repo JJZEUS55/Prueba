7e0256b076aa90d99a6584f5f3b6f0f6fd391079
JJZEUS55 <geoge55@hotmail.com>
2017-02-28 15:02:02 -0600

    nueva prueba

---
+++ src/pruebagit/prueba2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagit;
+
+/**
+ *
+ * @author JJZEUS55
+ */
+public class prueba2 {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Alo 2 papu de papus prros :v");
+    }
+    
+}
